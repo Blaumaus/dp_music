832682611178d194c81aeb344fa0666e4d96176a
Nayzorr <nazarkiriliuk3@gmail.com>
2021-04-18 21:26:04 +0300

    Added .NET structure

---
+++ API/DAL/Models/Selected.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DAL
+{
+    public partial class Selected
+    {
+        public string UserId { get; set; }
+        public string CompositionId { get; set; }
+
+        public virtual Composition Composition { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ API/DAL/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DAL
+{
+    public partial class User
+    {
+        public User()
+        {
+            Reflection = new HashSet<Reflection>();
+            Selected = new HashSet<Selected>();
+        }
+
+        public string Id { get; set; }
+        public string Login { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<Reflection> Reflection { get; set; }
+        public virtual ICollection<Selected> Selected { get; set; }
+    }
+}
