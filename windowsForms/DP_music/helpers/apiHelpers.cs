a1bc45459a997812588154cdfdb26febc0065809
DariaDziuba <dziuba.daria.anatolievna@gmail.com>
2021-05-23 18:14:47 +0300

    Add: previous WF

---
+++ windowsForms/DP_music/helpers/apiHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace DP_music.helpers
+{
+    public static class apiHelpers
+    {
+        private readonly static string basedURL = "https://localhost:44304/api/";
+
+        public static async Task<string> GetAll()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage res = await client.GetAsync(basedURL + "Genre"))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        var data = await content.ReadAsStringAsync();
+                        if (data != null)
+                        {
+                            return data;
+                        }
+                    }
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
